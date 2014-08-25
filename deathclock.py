aa0f5d08059be53a1b960a4dc2b876fdde740847
cameronmclean <ca.mclean@auckland.ac.nz>
2014-08-25 15:01:03 +1200

    looking at tkinter to make a GUI text/countdown

--- deathclock.py
+++ deathclock.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import time
+from Tkinter import *
 
 enddate = datetime(year=2015, month=8, day=31, hour=23, minute=59, second=59)
 now = datetime.now().replace(microsecond=0)
@@ -9,9 +10,20 @@ now = datetime.now().replace(microsecond=0)
 
 #diff = enddate - now
 
-while datetime.now() < enddate:
-	now = datetime.now().replace(microsecond=0)
-	diff = enddate - now
-	print diff
-	time.sleep(1)
+#root = Tk()
 
+def printTimeTillDeath():
+
+	while datetime.now() < enddate:
+		now = datetime.now().replace(microsecond=0)
+		diff = enddate - now
+		print diff
+		time.sleep(1)
+
+
+#gui = Label(root, text=printTimeTillDeath())
+#gui.pack()
+
+#root.mainloop()
+
+printTimeTillDeath()
